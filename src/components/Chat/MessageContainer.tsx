26f02ba2d8954d2d8f572d53b61c2ed5e8c6298b
Terry <i@terry.ee>
2023-06-06 19:40:00 +0800

    Update MessageContainer.tsx

--- src/components/Chat/MessageContainer.tsx
+++ src/components/Chat/MessageContainer.tsx
@@ -78,40 +78,6 @@ export default function ({
           />
         </Show>
       </div>
-      <Show
-        when={!store.loading && (store.contextToken || store.inputContentToken)}
-      >
-        <div class="flex items-center px-1em text-0.8em">
-          <hr class="flex-1 border-slate/40" />
-          <Show
-            when={store.inputContentToken}
-            fallback={
-              <span class="mx-1 text-slate/40">
-                {`有效上下文 Tokens : ${
-                  store.contextToken
-                }/$${store.contextToken$.toFixed(4)}`}
-              </span>
-            }
-          >
-            <span class="mx-1 text-slate/40">
-              {`有效上下文+提问 Tokens : ${
-                store.contextToken + store.inputContentToken
-              }(`}
-              <span
-                classList={{
-                  "text-red-500": store.remainingToken < 0
-                }}
-              >
-                {store.remainingToken}
-              </span>
-              {`)/$${(store.contextToken$ + store.inputContentToken$).toFixed(
-                4
-              )}`}
-            </span>
-          </Show>
-          <hr class="flex-1  border-slate/30" />
-        </div>
-      </Show>
     </div>
   )
 }
